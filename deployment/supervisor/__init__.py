00cd7425926f059d4c8a74fe30c671d71e6fc600
张建平 <seven_nighter@163.com>
2022-04-20 17:59:53 +0800

    目录结构调整.

---
+++ deployment/supervisor/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Mr_zhang
+@software: PyCharm
+@file: __init__.py.py
+@time: 2022/4/20 17:54
+"""
---
+++ start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#pipenv run python manage.py migrate
+#pipenv run python manage.py runserver 0.0.0.0:8000
+
+
+if [ $1 ]; then
+  if [ $1 = 'celery' ]; then
+    pipenv run python manage.py makemigrations
+    pipenv run python manage.py migrate
+    pipenv run celery -A face_safe worker -B -l info
+  elif [ $1 = 'celery-beta' ]; then
+    pipenv run python manage.py makemigrations
+    pipenv run python manage.py migrate
+    pipenv run celery -A face_safe beat --loglevel=INFO
+  elif [ $1 = 'celery-worker' ]; then
+    pipenv run python manage.py makemigrations
+    pipenv run python manage.py migrate
+    pipenv run celery -A face_safe worker -c 4 --loglevel=INFO
+  else
+    pipenv run python manage.py makemigrations
+    pipenv run python manage.py migrate
+    pipenv run python manage.py $1
+  fi
+else
+  echo "default"
+  pipenv run python manage.py makemigrations
+  pipenv run python manage.py migrate
+  export DJANGO_SUPERUSER_PASSWORD=Yishi@9086
+  pipenv run python manage.py createsuperuser --noinput --username Yishi  --email Yishi@localhost.com
+  pipenv run python manage.py runserver 0.0.0.0:8000
+fi
